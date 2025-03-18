c7d340f8cb2672faaa467613e645098817566922
MaksBukharskiy <maksbuharskij16@.com>
2025-03-18 15:03:44 +0500

    Fix some bugs

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -5,6 +5,10 @@ export default function FeedbackSection() {
   const [name, setName] = useState("");
   const [reason, setReason] = useState("help");
 
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
   function handleReasonChange(event) {
     setReason(event.target.value);
   }
@@ -27,7 +31,7 @@ export default function FeedbackSection() {
           id="reason" 
           className="control"
           value={reason}
-          onChange={handleReasonChange}
+          onChange={(event) => setReason(event.target.value)}
         >
           <option value="error">Ошибка</option>
           <option value="help">Нужна помощь</option>
